05428121cd254050148cebae4b67d6dbd940e01c
PauloAlexandreSantos <tecnicopaulo@outlook.pt>
2022-06-02 17:48:48 +0100

    routes v1

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,55 @@ use Illuminate\Support\Facades\Route;
 
 /* SITE */
 route::get('/', ['as' => 'site.home', 'uses' => 'HomeController@index']);
-/* noticias */
-Route::get('/noticias', ['as' => 'site.news', 'uses' => 'Site\NewsController@index']);
-Route::get('/noticia/{title}', ['as' => 'site.news.show', 'uses' => 'Site\NewsController@show']);
 
 
+/* people */
+Route::get('people/index', ['as' => 'people.index', 'uses' => 'PeopleController@index']);
+Route::get('people/show/{id}', ['as' => 'people.show', 'uses' => 'PeopleController@show']);
+
+
+
+
+/* Grupo de rotas autenticadas */
+
+Route::middleware(['auth'])->group(function () {
+    /* Manager Dashboard  */
+    route::get('painel', ['as' => 'home', 'uses' => 'DashboardController@index']);
+
+
+        /* User */
+        Route::get('user/index', ['as' => 'user.index', 'uses' => 'UserController@index']);
+        Route::get('user/show/{id}', ['as' => 'user.show', 'uses' => 'UserController@show']);
+
+        Route::get('user/edit/{id}', ['as' => 'user.edit', 'uses' => 'UserController@edit']);
+        Route::put('user/update/{id}', ['as' => 'user.update', 'uses' => 'UserController@update']);
+
+        Route::get('user/delete/{id}', ['as' => 'user.delete', 'uses' => 'UserController@destroy']);
+        /* end user */
+
+        /* contact */
+        Route::get('contact/create/{id}', ['as' => 'contact.create', 'uses' => 'ContactController@create']);
+        Route::post('contact/store/{id}', ['as' => 'contact.store', 'uses' => 'ContactController@store']);
+
+        Route::get('contact/edit/{id}', ['as' => 'contact.edit', 'uses' => 'ContactController@edit']);
+        Route::put('contact/update/{id}', ['as' => 'contact.update', 'uses' => 'ContactController@update']);
+
+        Route::get('contact/delete/{id}', ['as' => 'contact.delete', 'uses' => 'ContactController@destroy']);
+        /* End contact */
+
+        /* people */
+        Route::get('people/create', ['as' => 'people.create', 'uses' => 'PeopleController@create']);
+        Route::post('people/store', ['as' => 'people.store', 'uses' => 'PeopleController@store']);
+
+        Route::get('people/edit/{id}', ['as' => 'people.edit', 'uses' => 'PeopleController@edit']);
+        Route::put('people/update/{id}', ['as' => 'people.update', 'uses' => 'PeopleController@update']);
+
+        Route::get('people/delete/{id}', ['as' => 'people.delete', 'uses' => 'PeopleController@destroy']);
+        /* end people */
+
+
+});
 /* inclui as rotas de autenticação do ficheiro auth.php */
 require __DIR__ . '/auth.php';
 
-require __DIR__ . '/admin.php';
+
